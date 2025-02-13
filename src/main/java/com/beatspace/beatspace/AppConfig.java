b5d74c77a09153d28a84bf82a12bac334fe45361
João Marcos <joaomarcos77777.jm@gmail.com>
2025-02-13 18:12:37 -0300

    feat : adicionando endpoints no spotify controller

--- src/main/java/com/beatspace/beatspace/AppConfig.java
+++ src/main/java/com/beatspace/beatspace/AppConfig.java
@@ -10,8 +10,8 @@ public class AppConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE","PATCH").
-                        allowCredentials(true);
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
+                        .allowedHeaders("*");
             }
         };
     }
--- src/main/java/com/beatspace/beatspace/controllers/SpotifyController.java
+++ src/main/java/com/beatspace/beatspace/controllers/SpotifyController.java
@@ -8,10 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import java.io.IOException;
@@ -49,7 +47,6 @@ public class SpotifyController {
         // Codificação Base64 do client_id e client_secret
         String base64Auth = Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes(StandardCharsets.UTF_8));
 
-        System.out.println(base64Auth);
 
         // Constrói o corpo da requisição corretamente
         okhttp3.RequestBody body = new FormBody.Builder()
@@ -72,5 +69,134 @@ public class SpotifyController {
             return ResponseEntity.ok(response.body().string());
         }
     }
+
+
+    @GetMapping("/albuns/new-releases")
+    public ResponseEntity<?> getAlbuns(@RequestHeader("Authorization") String token) throws IOException{
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/browse/new-releases") // API do Spotify
+                .get()
+                .addHeader("Authorization",token)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                return ResponseEntity.status(response.code()).body(response.body().string());
+            }
+            return ResponseEntity.ok(response.body().string());
+        }
+    }
+
+    @GetMapping("/tracks/recommendations")
+    public ResponseEntity<?> getTracksRecommendations(@RequestHeader("Authorization") String token) throws IOException{
+        OkHttpClient client = new OkHttpClient();
+
+        // Criando a URL com query parameters
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https") // Protocolo (http ou https)
+                .host("api.spotify.com") // Host
+                .addPathSegment("v1/recommendations") // Caminho (ex: /dados)
+                .addQueryParameter("seed_genres", "classical,country") // Parâmetro 2
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA") // API do Spotify
+                .get()
+                .addHeader("Authorization",token)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                return ResponseEntity.status(response.code()).body(response.body().string());
+            }
+            return ResponseEntity.ok(response.body().string());
+        }
+    }
+
+    @GetMapping("/tracks/top")
+    public ResponseEntity<?> getMyTopTracks(@RequestHeader("Authorization") String token) throws IOException{
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me/top/tracks") // API do Spotify
+                .get()
+                .addHeader("Authorization",token)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                return ResponseEntity.status(response.code()).body(response.body().string());
+            }
+            return ResponseEntity.ok(response.body().string());
+        }
+    }
+
+    @GetMapping("/artists/top")
+    public ResponseEntity<?> getMyTopArtists(@RequestHeader("Authorization") String token) throws IOException{
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me/top/artists") // API do Spotify
+                .get()
+                .addHeader("Authorization",token)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                return ResponseEntity.status(response.code()).body(response.body().string());
+            }
+            return ResponseEntity.ok(response.body().string());
+        }
+    }
+
+    @GetMapping("/user/current")
+    public ResponseEntity<?> getCurrentUser(@RequestHeader("Authorization") String token) throws IOException{
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me") // API do Spotify
+                .get()
+                .addHeader("Authorization",token)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                return ResponseEntity.status(response.code()).body(response.body().string());
+            }
+            return ResponseEntity.ok(response.body().string());
+        }
+    }
+
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchItem(@RequestHeader("Authorization") String token,@RequestParam("type") String type ,@RequestParam("query") String query) throws IOException{
+                OkHttpClient client = new OkHttpClient();
+                // Criando a URL com query parameters
+                HttpUrl url = new HttpUrl.Builder()
+                        .scheme("https") // Protocolo (http ou https)
+                        .host("api.spotify.com") // Host
+                        .addPathSegment("v1")
+                        .addPathSegment("search")
+                        .addQueryParameter("type",type)
+                        .addQueryParameter("q", query) // Parâmetro 2
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url(url) // API do Spotify
+                        .get()
+                        .addHeader("Authorization",token)
+                        .build();
+
+
+                try (Response response = client.newCall(request).execute()) {
+                    if (!response.isSuccessful()) {
+                        return ResponseEntity.status(response.code()).body(response.body().string());
+                    }
+                    return ResponseEntity.ok(response.body().string());
+                }
+            }
 
 }
