e8a63ac5e39193c7bb8a03d79a47b8e2ab587fbe
Juliana Ferreira <julianafercavalcante@hotmail.com>
2025-03-18 20:51:26 -0300

    refactor: refatorando UserAction

--- src/main/java/com/beatspace/beatspace/models/UserAction.java
+++ src/main/java/com/beatspace/beatspace/models/UserAction.java
@@ -8,37 +8,73 @@ import java.time.LocalDateTime;
 
 
 @Entity
-@Table(name= "user_actions")
 public class UserAction {
 
-
-    @Getter
-    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Getter
-    @Setter
     private String userId;
-    @Getter
-    @Setter
     private String username;
-    @Getter
-    @Setter
     private String action;
-    @Getter
-    @Setter
     private String details;
-
-    @Getter
-    @Setter
     private LocalDateTime timestamp;
 
+    // Getter e Setter para id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter e Setter para userId
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    // Getter e Setter para username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter e Setter para action
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    // Getter e Setter para details
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    // Getter e Setter para timestamp
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 
     @PrePersist
     protected void onCreate() {
         timestamp = LocalDateTime.now();
     }
-
 }
